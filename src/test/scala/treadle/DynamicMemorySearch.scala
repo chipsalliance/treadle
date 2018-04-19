c8aab13d0519b8adf85242fc89087288a90cd2cf
chick <chick@qrhino.com>
2018-04-19 15:25:50 -0700

    Continuing refactor of register handling
    * DynamicMemoryTest working again

--- src/test/scala/treadle/DynamicMemorySearch.scala
+++ src/test/scala/treadle/DynamicMemorySearch.scala
@@ -49,17 +49,20 @@ class DynamicMemorySearch extends FreeSpec with Matchers {
       |    io.target <= index
     """.stripMargin
 
-    val n = 4
+    // these numbers are hard-code into above firrtl, do not change with out fixing
+    val n = 8
     val w = 4
+
+    val showLocalDebug = false
 
     val optionsManager = new TreadleOptionsManager {
       treadleOptions = treadleOptions.copy(
         writeVCD = false,
         vcdShowUnderscored = false,
-        setVerbose = true,
+        setVerbose = false,
         showFirrtlAtLoad = true,
         rollbackBuffers = 0,
-        symbolsToWatch = Seq() // Seq("dut.io_host_tohost", "dut.dpath.csr.mtohost")
+        symbolsToWatch = Seq()
       )
     }
 
@@ -99,73 +102,82 @@ class DynamicMemorySearch extends FreeSpec with Matchers {
 
       // initialize memory
       for(write_address <- 0 until n) {
-        println(s"Initializing memory address $write_address")
         poke("io_en", 1)
         poke("io_isWr", 1)
         poke("io_wrAddr", write_address)
-        poke("io_data",   write_address + 3)
+        poke("io_data",   write_address)
         list(write_address) = write_address
         step()
+        println(s"Initializing memory address $write_address with $write_address mem($write_address)" +
+                s" is ${peekMemory("list", write_address)}")
       }
 
-      (0 until n).foreach { i =>
-        val mem = peekMemory("list", i)
-        println(s"$i : $mem")
+      if(showLocalDebug) {
+        println(s"Memory init done XXXXXXXXXX")
+        (0 until n).foreach { i =>
+          val mem = peekMemory("list", i)
+          println(s"$i : $mem ${list(i)}")
+        }
       }
 
-//      startSearch(4)
-//      checkSearch(4)
+      startSearch(4)
+      checkSearch(4)
 
-//
-//
-//      // println(s"${engine.circuitState.memories("list").toString}")
-//
-//      for (k <- 0 until 160) {
-//        println(s"memory test iteration $k") // ${"X"*80}")
-//
-//        // Compute a random address and value
-//        val wrAddr = random.nextInt(n - 1)
-//        val data   = random.nextInt((1 << w) - 1)
-//        println(s"setting memory($wrAddr) = $data")
-//
-//        // poke it into memory
-//        poke("io_en", 0)
-//        poke("io_isWr", 1)
-//        poke("io_wrAddr", wrAddr)
-//        poke("io_data",   data)
-//        list(wrAddr) = data
-//        step()
-//
-//        // SETUP SEARCH
-//        val target = if (k > 12) random.nextInt(1 << w) else data
-//        poke("io_isWr", 0)
-//        poke("io_data", target)
-//        poke("io_en", 1)
-//        step()
-//        poke("io_en", 0)
-//        val expectedIndex = if (list.contains(target)) {
-//          list.indexOf(target)
-//        } else {
-//          list.length - 1
-//        }
-//        // println(s"test pass $k ${this.engine.circuitState.prettyString()}")
-//
-//        var waitCount = 0
-//        while(waitCount <= n && peek("io_done") == Big0) {
-//          // println(s"Waiting for done $waitCount")
-//          // println(this.engine.circuitState.prettyString())
-//          // println(s"external list ${list.mkString(",")}")
-//          step()
-//          waitCount += 1
-//        }
-//
-//        // println(s"Done wait count is $waitCount done is ${peek("io_done")} " +
-//        //   s"got ${peek("io_target")} got $expectedIndex")
-//        expect("io_done", 1)
-//        expect("io_target", expectedIndex)
-//        step()
-//
-//      }
+      for (k <- 0 until 160) {
+        println(s"memory test iteration $k") // ${"X"*80}")
+
+        // Compute a random address and value
+        val wrAddr = random.nextInt(n - 1)
+        val data   = random.nextInt((1 << w) - 1)
+        if(showLocalDebug) {
+          println(s"setting memory($wrAddr) = $data")
+        }
+
+        // poke it into memory
+        poke("io_en", 0)
+        poke("io_isWr", 1)
+        poke("io_wrAddr", wrAddr)
+        poke("io_data",   data)
+        list(wrAddr) = data
+        step()
+
+        if(showLocalDebug) {
+          println(s"current memory ")
+          println((0 until n).map { i => s"$i:${peekMemory("list", i)}:${list(i)}" }.mkString(", "))
+          println()
+        }
+
+        // SETUP SEARCH
+        val target = if (k > 12) random.nextInt(1 << w) else data
+        poke("io_isWr", 0)
+        poke("io_data", target)
+        poke("io_en", 1)
+        step()
+        poke("io_en", 0)
+        val expectedIndex = if (list.contains(target)) {
+          list.indexOf(target)
+        } else {
+          list.length - 1
+        }
+
+        var waitCount = 0
+        while(waitCount <= n && peek("io_done") == Big0) {
+          // println(s"Waiting for done $waitCount")
+          // println(this.engine.circuitState.prettyString())
+          // println(s"external list ${list.mkString(",")}")
+          step()
+          waitCount += 1
+        }
+
+        if(showLocalDebug) {
+          println(s"Done wait count is $waitCount done is ${peek("io_done")} " +
+                  s"got ${peek("io_target")} got $expectedIndex")
+        }
+        expect("io_done", 1)
+        expect("io_target", expectedIndex)
+        step()
+
+      }
       report()
     }
   }
