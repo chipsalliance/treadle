d72fe634835fa2e3d8301d1b2c2c1a9a73400590
Chick Markley <chick@qrhino.com>
2018-11-27 14:15:36 -0800

    Better clock handling (#65)
    * Better clock handling
    - Add advanceTime method to API for precise control of multi-clock DUT
    - If signal poked a second time without time or clock advance, evaluate circuit
    - Fix scalastyle in engine
    - Check transition on triggered assign, and update if up transition
    - Add tests for use of advanceTime API
    
    * Better clock handling
    - Make the wall clocks scale name visible
    - provide scaladoc for new API method advanceTime
    - removed no longer used test.

--- src/main/scala/treadle/TreadleTester.scala
+++ src/main/scala/treadle/TreadleTester.scala
@@ -80,6 +80,17 @@ class TreadleTester(input: String, optionsManager: HasTreadleSuite = new Treadle
       new MultiClockStepper(engine = this.engine, clockInfoList, wallTime)
   }
 
+  /**
+    * Advance time in ticks of the [[UTC]] wallTime, the default is picoseconds, but can be
+    * read by the scaleName of the wallTime.  One should probably be advancing by some simple factor
+    * of a clock period. The clockInfoList of the options should define this (could be more than one).
+    * @param interval units are in units of the [[wallTime]] scale.
+    */
+  def advanceTime(interval: Long): Unit = {
+    assert(interval >= 0L, "TreadleTester#advanceTime called with negative value")
+    wallTime.setTime(wallTime.currentTime + interval)
+  }
+
   /*
   The Idea here is that combinational delay will be used when a peek follows a poke without a step
   This should allow VCD output to show the events as if they had taken place in a small
