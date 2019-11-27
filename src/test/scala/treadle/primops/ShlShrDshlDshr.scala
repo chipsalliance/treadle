0de62d836c953a02ea0e6f0b5b416a9d26a77fe2
Chick Markley <chick@qrhino.com>
2019-11-27 10:20:36 -0800

    Merge pull request #154 from freechipsproject/riscv-mini-fixes
    Fixes for problem discovered in riscv-mini
    
    (cherry picked from commit 5e7e4801b0760d37530abc456fcad409f07e940e)

--- src/test/scala/treadle/primops/ShlShrDshlDshr.scala
+++ src/test/scala/treadle/primops/ShlShrDshlDshr.scala
@@ -2,10 +2,10 @@
 
 package treadle.primops
 
+import firrtl.stage.FirrtlSourceAnnotation
 import treadle.executable._
-import treadle.{BitTwiddlingUtils, extremaOfSIntOfWidth, extremaOfUIntOfWidth}
+import treadle.{BitTwiddlingUtils, ShowFirrtlAtLoadAnnotation, TreadleTester, VerboseAnnotation, extremaOfSIntOfWidth, extremaOfUIntOfWidth}
 import org.scalatest.{FreeSpec, Matchers}
-
 
 // scalastyle:off magic.number
 class ShlShrDshlDshr extends FreeSpec with Matchers {
@@ -31,6 +31,48 @@ class ShlShrDshlDshr extends FreeSpec with Matchers {
         staticShifter() should be (staticExpected)
         dynamicShifter() should be (staticExpected)
       }
+    }
+
+    "dynamic shift with constant" in {
+      val input =
+        """
+          |circuit ShiftTest :
+          |  module ShiftTest :
+          |    input clk : Clock
+          |    input reset : UInt<1>
+          |    input  intInput    : UInt<30>
+          |    input  longInput   : UInt<60>
+          |
+          |    output intToLong    : UInt<90>
+          |    output intToBig     : UInt<128>
+          |
+          |    output longToBig    : UInt<90>
+          |
+          |    intToLong    <= dshl(intInput, UInt<7>(32))
+          |    intToBig     <= dshl(intInput, UInt<7>(60))
+          |
+          |    longToBig    <= dshl(longInput, UInt<7>(32))
+
+      """.stripMargin
+
+      val t = TreadleTester(Seq(FirrtlSourceAnnotation(input)))
+
+      val intInput = BigInt( "1234", 16)
+      val longInput = BigInt("123456789abc", 16)
+      t.poke("intInput", intInput)
+      t.poke("longInput", longInput)
+
+      val intToLong: BigInt = t.peek("intToLong")
+      val intToBig  = t.peek("intToBig")
+      val longToBig = t.peek("longToBig")
+
+      println(f"intInput  ${intInput.toString(16)} << 32 yields long ${intToLong.toString(16)} with ${intToLong.bitLength}")
+      println(f"intInput  ${intInput.toString(16)} << 60 yields big  ${intToBig.toString(16)}  with ${intToBig.bitLength}")
+      println(f"longInput ${intInput.toString(16)} << 30 yields big  ${longToBig.toString(16)} with ${longToBig.bitLength}")
+
+      t.expect("intToLong", BigInt("123400000000", 16))
+      t.expect("intToBig",  BigInt("1234000000000000000", 16))
+      t.expect("longToBig", BigInt("123456789abc00000000", 16))
     }
 
     "Using UInts" in {
