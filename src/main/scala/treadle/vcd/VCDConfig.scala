f19a23ed00a552ea124a8008edfd99018f3a15a5
chick <chick@qrhino.com>
2019-12-19 16:45:04 -0800

    fixed format on vcd config

--- src/main/scala/treadle/vcd/VCDConfig.scala
+++ src/main/scala/treadle/vcd/VCDConfig.scala
@@ -4,16 +4,14 @@ package treadle.vcd
 
 import firrtl.ExecutionOptionsManager
 
-case class VCDConfig(
-                     vcdSourceName:       String = "",
-                     vcdTargetName:       String = "",
-                     startScope:          String = "",
-                     renameStartScope:    String = "",
-                     varPrefix:           String = "",
-                     newVarPrefix:        String = "",
-                     dumpHumanReadable:   Boolean = false)
-  extends firrtl.ComposableOptions {
-}
+case class VCDConfig(vcdSourceName:     String = "",
+                     vcdTargetName:     String = "",
+                     startScope:        String = "",
+                     renameStartScope:  String = "",
+                     varPrefix:         String = "",
+                     newVarPrefix:      String = "",
+                     dumpHumanReadable: Boolean = false)
+    extends firrtl.ComposableOptions {}
 
 trait HasVCDConfig {
   self: ExecutionOptionsManager =>
@@ -22,32 +20,48 @@ trait HasVCDConfig {
 
   parser.note("vcd")
 
-  parser.opt[String]("start-scope")
+  parser
+    .opt[String]("start-scope")
     .abbr("vss")
-    .foreach { x => vcdConfig = vcdConfig.copy(startScope = x)}
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(startScope = x)
+    }
     .text("starts saving information at specified scope")
 
-  parser.opt[String]("rename-start-scope")
+  parser
+    .opt[String]("rename-start-scope")
     .abbr("vrss")
-    .foreach { x => vcdConfig = vcdConfig.copy(renameStartScope = x)}
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(renameStartScope = x)
+    }
     .text("rename startScope to this")
 
-  parser.opt[Unit]("dump-human-readable")
+  parser
+    .opt[Unit]("dump-human-readable")
     .abbr("vdhr")
-    .foreach { x => vcdConfig = vcdConfig.copy(dumpHumanReadable = true)}
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(dumpHumanReadable = true)
+    }
     .text("rename startScope to this")
 
-  parser.opt[String]("retain-vars-with-prefix")
+  parser
+    .opt[String]("retain-vars-with-prefix")
     .abbr("vrp")
-    .foreach { x => vcdConfig = vcdConfig.copy(varPrefix = x) }
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(varPrefix = x)
+    }
     .text("only vars that start with prefix will be kept")
 
-  parser.opt[String]("new-var-prefix...")
+  parser
+    .opt[String]("new-var-prefix...")
     .abbr("vnvp")
-    .foreach { x => vcdConfig = vcdConfig.copy(newVarPrefix = x) }
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(newVarPrefix = x)
+    }
     .text("re-prefix vars with this string")
 
-  parser.opt[String]("vcd-input")
+  parser
+    .opt[String]("vcd-input")
     .abbr("vi")
     .required()
     .foreach { x =>
@@ -55,9 +69,12 @@ trait HasVCDConfig {
     }
     .text("name of input vcd file")
 
-  parser.arg[String]("<output-vcd-file>...")
+  parser
+    .arg[String]("<output-vcd-file>...")
     .optional()
-    .foreach { x => vcdConfig = vcdConfig.copy(vcdTargetName = x) }
+    .foreach { x =>
+      vcdConfig = vcdConfig.copy(vcdTargetName = x)
+    }
     .text("name of output vcd file (optional)")
 }
 
