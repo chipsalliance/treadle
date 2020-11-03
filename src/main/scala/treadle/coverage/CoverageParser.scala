37e4a509413c6c6877d705b3409dfaa3b45850bd
Andrew Dobis <andrew.dobis@epfl.ch>
2020-11-03 15:36:15 +0100

    newline at eof

--- src/main/scala/treadle/coverage/CoverageParser.scala
+++ src/main/scala/treadle/coverage/CoverageParser.scala
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 DTU Compute Embedded Systems Section
+Copyright 2020 Danmarks Tekniske Universitet
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
