23fb284fa4154b63b34de9bc9df34ff1f93ea599
Andrew Dobis <andre.dobis@gmail.com>
2020-11-11 16:16:32 +0100

    WIP: Added additional coverage ports in firrtl pass

--- src/main/scala/treadle/coverage/pass/AddCoverageExpressions.scala
+++ src/main/scala/treadle/coverage/pass/AddCoverageExpressions.scala
@@ -2,11 +2,15 @@
 package treadle.coverage.pass
 
 import firrtl.{Namespace, Utils}
-import firrtl.ir.{Circuit, Module, ExtModule, DefModule, IntWidth, NoInfo, Output, Port, Statement, UIntType}
+import firrtl.ir.{Block, Circuit, Connect, DefModule, Expression, ExtModule, HasInfo, Info, IntWidth, Module, Mux, NoInfo, Output, Port, Reference, Statement, UIntLiteral, UIntType}
 import firrtl.passes.Pass
 import firrtl.stage.TransformManager.TransformDependency
+import treadle.coverage.pass.AddCoverageExpressions.coverageName
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+
 
 /**
   * Adds additional coverage statements to the Low FIRRTL source at every mux location.
@@ -31,14 +35,12 @@ object AddCoverageExpressions extends Pass {
     def coverM(ledger: Ledger)(module: DefModule): DefModule = {
         val namespace = Namespace(module)
         val newModule = module mapStmt coverS(ledger, namespace)
-        val newPorts = newModule.ports ++ (for (_ <- 0 until ledger.nMux(module.name))
-            yield Port(NoInfo, namespace.newName(coverageName), Output, UIntType(IntWidth(1))))
+        val newPorts = newModule.ports ++ ledger.ports
 
         //Add new ports to the module
         newModule match {
             case mod: Module => mod.copy(ports = newPorts)
             case extMod: ExtModule => extMod.copy(ports = newPorts)
-            case _ => _
         }
     }
 
@@ -46,14 +48,56 @@ object AddCoverageExpressions extends Pass {
       * TODO: Traverse statements, find muxes and insert coverage ports there
       */
     def coverS(ledger: Ledger, namespace: Namespace)(s: Statement) : Statement = {
-        s
+        val block = mutable.ArrayBuffer[Statement]()
+        s match {
+            case s: HasInfo =>
+                val newStmt = s.mapExpr(coverE(ledger, namespace, block, s.info))
+                block.length match {
+                    case 0 => newStmt
+                    case _ => Block(block :+ newStmt)
+                }
+            case s => s.mapStmt(coverS(ledger, namespace))
+        }
     }
+
+    def coverE(ledger: Ledger, namespace: Namespace, block: mutable.ArrayBuffer[Statement], info: Info)(e: Expression): Expression =
+        e.mapExpr(coverE(ledger, namespace, block, info)) match {
+            //Look for muxes, if we find one, add a coverage statement
+            case Mux(cond, _, _, _) =>
+                ledger.foundMux()
+                val ref1 = ledger.addCoveragePort(namespace)
+                val ref2 = ledger.addCoveragePort(namespace)
+                block += Connect(NoInfo, ref1, cond)
+                //block += Connect(NoInfo, ref2, notCond)
+                e
+
+            case notmux => notmux
+        }
 }
 
 class Ledger {
     private var moduleName: Option[String] = None
     private val modules = mutable.Set[String]()
     private val moduleMuxMap = mutable.Map[String, Int]()
+    private val coveragePorts = mutable.ArrayBuffer[Port]()
+    private var curCovName: Int = 0
+
+    def ports: Seq[Port] = coveragePorts
+
+    def nextCoveragePort: Port =
+        if(coveragePorts.length <= curCovName) throw new IllegalAccessException("No more names to use")
+        else coveragePorts({
+                val oldName = curCovName
+                curCovName += 1
+                oldName
+            })
+
+    def addCoveragePort(namespace: Namespace): Reference = {
+        val port = Port(NoInfo, namespace.newName(coverageName), Output, UIntType(IntWidth(1)))
+        coveragePorts += port
+        Reference(port)
+    }
+
 
     def foundMux(): Unit = moduleName match {
         case None       => sys.error("Module name not defined in Ledger!")
